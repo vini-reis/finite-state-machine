ba44c64101061554b3bae201fcd7aec8303d151e
Vinícius Reis <vnc.reis@outlook.com>
2023-04-15 19:30:48 -0300

    [WIP] Making FSM types generic

--- fsm/src/main/kotlin/StateMachine.kt
+++ fsm/src/main/kotlin/StateMachine.kt
@@ -6,12 +6,20 @@ class StateMachine<IState : Any, IEvent : Any, ISideEffect : Any> private constr
     private lateinit var finalState: IState
     private lateinit var onTransition: (ISideEffect) -> Unit
     private lateinit var currentState: IState
-    private val transitionsMap: HashMap<IState, MutableList<Transition>> = hashMapOf()
+    private val transitionsMap: HashMap<IState, MutableList<Transition<IEvent, IState, ISideEffect>>> = hashMapOf()
 
-    class Transition(val on: IEvent, val to: IState, val effect: ISideEffect) {
-        class Valid(val from: IState, val on: IEvent, val to: IState, val effect: ISideEffect)
+    sealed class Transition<FS, E> {
+        abstract val from: FS
+        abstract val on: E
 
-        class Invalid(val from: IState, val on: IEvent)
+        data class Valid<FS, E, TS, SE>(
+            override val from: FS,
+            override val on: E,
+            val to: TS,
+            val effect: SE
+        ) : Transition<FS, E>()
+
+        data class Invalid<FS, E>(override val from: FS, override val on: E) : Transition<FS, E>()
     }
 
     open inner class New internal constructor(open val name: String) {
@@ -41,7 +49,7 @@ class StateMachine<IState : Any, IEvent : Any, ISideEffect : Any> private constr
 
     open inner class WithFinalState internal constructor() : WithInitialState()
     {
-        fun state(state: IState, builder: StateBuilder.() -> Transition) {
+        fun state(state: IState, builder: StateBuilder.() -> Transition<IEvent, IState, ISideEffect>) {
             transitionsMap.getOrPut(state) { mutableListOf() }.add(builder(StateBuilder()))
         }
 
@@ -53,7 +61,7 @@ class StateMachine<IState : Any, IEvent : Any, ISideEffect : Any> private constr
     }
 
     inner class StateBuilder {
-        fun on(event: IEvent, builder: TransitionBuilder.() -> Transition) = builder(TransitionBuilder(event))
+        fun on(event: IEvent, builder: TransitionBuilder.() -> Transition<IEvent, IState, ISideEffect>) = builder(TransitionBuilder(event))
     }
 
     inner class TransitionBuilder(private val on: IEvent){
@@ -61,20 +69,22 @@ class StateMachine<IState : Any, IEvent : Any, ISideEffect : Any> private constr
     }
 
     fun fire(e: IEvent){
-        findTransition(e).also {
-
-
-            logger.info("Event ${e::class.simpleName} fired!")
-            logger.info("Starting side effect ${it.effect::class.simpleName}...")
-            onTransition(it.effect)
-            logger.info("Side effect ${it.effect::class.simpleName} finished!")
-            logger.info("Transiting to ${currentState::class.simpleName} -> ${it.to::class.simpleName}")
-            currentState = it.to
+        findTransition(currentState, e).let {
+            when(it) {
+                is Transition.Valid<*, *, *, *> -> {
+                    logger.info("Event ${e::class.simpleName} fired!")
+                    logger.info("Starting side effect ${it.effect::class.simpleName}...")
+                    onTransition(it.effect)
+                    logger.info("Side effect ${it.effect::class.simpleName} finished!")
+                    logger.info("Transiting to ${currentState::class.simpleName} -> ${it.to::class.simpleName}")
+                    currentState = it.to
+                }
+            }
         }
     }
 
-    private fun findTransition(e: IEvent): Transition =
-        transitionsMap.getOrElse(currentState) { listOf() }.firstOrNull { it.on == e }?.let {
+    private fun <IState> findTransition(from: IState, e: IEvent): Transition<IState, IEvent> =
+        transitionsMap.getOrElse(from) { listOf() }.firstOrNull { it.on == e }?.let {
             Transition.Valid(currentState, e, it.to, it.effect)
         } ?: Transition.Invalid(currentState, e)
 

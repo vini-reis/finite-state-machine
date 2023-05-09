7a6efe79e766b4a9821e457994bae1faa0cf6b2a
Vinícius Reis <vnc.reis@outlook.com>
2023-05-08 21:28:34 -0300

    Add current state and event on EventHandler callbacks

--- src/main/kotlin/EventHandler.kt
+++ src/main/kotlin/EventHandler.kt
@@ -1,9 +1,9 @@
 import java.util.logging.Logger
 
 abstract class EventHandler<S : Any, E : Any, SE : Any, C : Any> {
-    abstract fun validate(controller: StateMachine<S, E, SE, C>.Controller, context: C) : Result
-    abstract suspend fun handle(controller: StateMachine<S, E, SE, C>.Controller, context: C)
-    abstract fun error(controller: StateMachine<S, E, SE, C>.Controller, context: C)
+    abstract fun validate(controller: StateMachine<S, E, SE, C>.Controller, context: C, state: S, event: E) : Result
+    abstract suspend fun handle(controller: StateMachine<S, E, SE, C>.Controller, context: C, state: S, event : E)
+    abstract fun error(controller: StateMachine<S, E, SE, C>.Controller, context: C, state: S, event: E)
     abstract fun exception(controller: StateMachine<S, E, SE, C>.Controller, e: Exception, context: C)
 
     sealed class Result {
@@ -11,15 +11,14 @@ abstract class EventHandler<S : Any, E : Any, SE : Any, C : Any> {
         object Invalid : Result()
     }
 
-    internal suspend fun execute(scope: StateMachine<S, E, SE, C>.Controller, context: C){
+    internal suspend fun execute(scope: StateMachine<S, E, SE, C>.Controller, context: C, state: S, event: E){
         try {
-            when(validate(scope, context)) {
-                is Result.Valid -> handle(scope, context)
-                is Result.Invalid -> error(scope, context)
+            when(validate(scope, context, state, event)) {
+                is Result.Valid -> handle(scope, context, state, event)
+                is Result.Invalid -> error(scope, context, state, event)
             }
         } catch (e: Exception) {
             logger.severe(e.message)
-            e.printStackTrace()
             exception(scope, e, context)
         }
     }
@@ -28,23 +27,29 @@ abstract class EventHandler<S : Any, E : Any, SE : Any, C : Any> {
         private val logger = Logger.getLogger(this::class.simpleName)
 
         fun <S : Any, E : Any, SE : Any, C : Any> build(
-            handler: suspend (StateMachine<S, E, SE, C>.Controller, C) -> Unit
+            handler: suspend (StateMachine<S, E, SE, C>.Controller, C, S, E) -> Unit
         ) = object : EventHandler<S, E, SE, C>() {
             override fun validate(
                 controller: StateMachine<S, E, SE, C>.Controller,
-                context: C
+                context: C,
+                state: S,
+                event: E
             ): Result = Result.Valid
 
             override suspend fun handle(
                 controller: StateMachine<S, E, SE, C>.Controller,
-                context: C
+                context: C,
+                state: S,
+                event: E
             ) {
-                handler(controller, context)
+                handler(controller, context, state, event)
             }
 
             override fun error(
                 controller: StateMachine<S, E, SE, C>.Controller,
-                context: C
+                context: C,
+                state: S,
+                event: E
             ) {
                 throw IllegalStateException("This event handler should not fail!")
             }
